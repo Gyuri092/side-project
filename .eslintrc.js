845d228c8676c9c72bb97a06cf6eb4d1c9dd1bfb
GyuriGwon <ggr00000@gmail.com>
2023-09-07 13:00:24 +0900

    init: 모노레포 프로젝트 세팅중(1) - eslint, prettier, gitignore 설정, turborepo 설치

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  ignorePatterns: ['node_modules/', 'dist/'],
+  extends: [
+    'airbnb', // or airbnb-base
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          //   '.jsx',
+          '.ts',
+          //   '.tsx',
+        ],
+      },
+    },
+  },
+  root: true,
+};
